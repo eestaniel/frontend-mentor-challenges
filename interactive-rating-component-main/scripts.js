28a8fb85a8897a4729ec417df9fc09b9956968ae
EEstaniel <edwineestaniel93@gmail.com>
2023-05-20 01:53:16 -0700

    fix: update result score on thank you page

--- interactive-rating-component-main/scripts.js
+++ interactive-rating-component-main/scripts.js
@@ -2,7 +2,7 @@ const button = document.getElementsByClassName('button')[0]
 const card = document.getElementsByClassName('card')[0]
 const submit_card = document.getElementsByClassName('submit-card')[0]
 const scores = document.querySelectorAll('.score')
-
+let doc_score = document.getElementById('_score')
 let did_submit = false;
 let selected_score = 0;
 
@@ -20,5 +20,6 @@ button.addEventListener('click', () => {
     if (selected_score !== 0) {
         card.classList.toggle('hidden')
         submit_card.classList.toggle('hidden')
+        doc_score.innerText = selected_score
     }
 })
