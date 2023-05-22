c18e84d89ea5bbf8a26ae3c0c54eded7fd2898a3
EEstaniel <edwineestaniel93@gmail.com>
2023-05-22 15:54:00 -0700

    fix: fixed spacing and images for desktop view

--- faq-accordion-card/src/components/Accordion.tsx
+++ faq-accordion-card/src/components/Accordion.tsx
@@ -42,16 +42,12 @@ const Accordion = () => {
             <div className="accordion">
 
                 <div className="bg">
-                    <div className="bg__computer"></div>
-                    <div className="bg__box"></div>
-                    <div className="bg__mobile"></div>
-                    <div className="bg__mobile_shadow"></div>
                     <div className="bg__mobile__computer"></div>
-                    <svg className={'bg__shadow'} width="240" height="104" xmlns="http://www.w3.org/2000/svg"><path d="M116.454 102.048L4.667 34.239c-3.56-2.054-6.61-5.42-3.142-7.627l112.184-66.364c1.682-1.07 4.237-1.773 5.951-.75l117.328 69.904c3.559 2.12 4.198 8.028.652 10.17L131.185 101.94a14.53 14.53 0 01-14.731.107z" fill="#6862E6" fill-rule="nonzero" opacity=".099"/></svg>
+                    <div className="bg__mobile__shadow"></div>
+                    <div className="bg__mobile__computer"></div>
                     <div className="bg__desktop__computer"></div>
 
                     <div className="bg__desktop__shadow"></div>
-
                 </div>
 
 
@@ -72,7 +68,9 @@ const Accordion = () => {
                             </div>
                             {selectedQuestionIndex === item.index && <div className={'answer'}>{item.answer}</div>}
                             <hr/>
+
                         </div>
+
                     ))}
                 </div>
                 </div>
