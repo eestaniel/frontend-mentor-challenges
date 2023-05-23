654aa3ea04ebe68cff6b004416581fc21bfe0018
EEstaniel <edwineestaniel93@gmail.com>
2023-05-23 04:54:32 -0700

    fix: added mobile active states

--- article-preview-component/scripts.js
+++ article-preview-component/scripts.js
@@ -3,14 +3,20 @@ const share_icon = document.querySelector(".article__share-icon-color");
 const share_box = document.querySelector(".share__flex_group");
 const square = document.querySelector(".square");
 
+let desktop = window.matchMedia("(min-width: 500px)");
 
 
 function handleShareButton() {
     share_box.classList.toggle("hidden");
     square.classList.toggle("hidden");
-    share_button.classList.toggle("toggle");
     share_icon.classList.toggle("toggle");
+    share_button.classList.toggle("toggle");
 
+
+    if (!desktop.matches) {
+        share_button.classList.toggle("share__toggle");
+
+    }
 }
 
 
