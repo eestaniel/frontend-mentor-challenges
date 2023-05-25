e13c790150343c85016260f1e9bf07c93f9e0093
EEstaniel <edwineestaniel93@gmail.com>
2023-05-25 03:04:13 -0700

    fix: updated conditional for email input

--- intro-component-with-signup-form-master/scripts.js
+++ intro-component-with-signup-form-master/scripts.js
@@ -17,41 +17,39 @@ document.addEventListener("DOMContentLoaded", function(){
             // get the parent element
             const parent = input.parentElement;
 
+            // remove error text and icon if they exist
+            const oldErrorText = parent.querySelector(".form__error-text");
+            const oldErrorIcon = parent.querySelector(".form__error-icon");
+
+            if(oldErrorText){
+                parent.removeChild(oldErrorText);
+            }
+            if(oldErrorIcon){
+                parent.removeChild(oldErrorIcon);
+            }
+
             // check if input is empty or email is not valid
             if(input.value === "" || (input === emailInput && !validateEmail(input.value))){
-                // add error text and icon if not already added
-                if(!parent.querySelector(".form__error-text")){
-                    // create and append error text
-                    const errorText = document.createElement("div");
-                    errorText.classList.add("form__error-text");
-                    if(input.value === ""){
-                        errorText.innerText = input.placeholder + " cannot be empty!";
-                    } else if(input === emailInput){
-                        errorText.innerText = "Looks likes this is an invalid email.";
-                    }
-                    parent.appendChild(errorText);
-
-                    // create and append error icon
-                    const errorIcon = document.createElement("div");
-                    errorIcon.classList.add("form__error-icon");
-                    parent.appendChild(errorIcon);
+                // add error text and icon
+                // create and append error text
+                const errorText = document.createElement("div");
+                errorText.classList.add("form__error-text");
+                if(input.value === ""){
+                    errorText.innerText = input.placeholder + " cannot be empty!";
+                } else if(input === emailInput && !validateEmail(input.value)){
+                    errorText.innerText = "Looks likes this is an invalid email.";
                 }
+                parent.appendChild(errorText);
+
+                // create and append error icon
+                const errorIcon = document.createElement("div");
+                errorIcon.classList.add("form__error-icon");
+                parent.appendChild(errorIcon);
 
                 // Apply the 'input-invalid' class.
                 input.classList.remove('input-valid');
                 input.classList.add('input-invalid');
             } else {
-                // remove error text and icon if they exist
-                const errorText = parent.querySelector(".form__error-text");
-                const errorIcon = parent.querySelector(".form__error-icon");
-
-                if(errorText){
-                    parent.removeChild(errorText);
-                }
-                if(errorIcon){
-                    parent.removeChild(errorIcon);
-                }
-
                 // Apply the 'input-valid' class.
                 input.classList.remove('input-invalid');
                 input.classList.add('input-valid');
