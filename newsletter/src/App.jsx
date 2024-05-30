0710a1bb87b35048c37f6e9368f46875bbf2f21c
EEstaniel <edwineestaniel93@gmail.com>
2024-05-30 00:03:00 -0700

    feat: add state for email

--- newsletter/src/App.jsx
+++ newsletter/src/App.jsx
@@ -1,18 +1,23 @@
 import './globals.css';
 import Newsletter from "./components/Newsletter";
-
-
-
+import {useState} from "react";
 
 
 function App() {
+  const [email, setEmail] = useState('');
 
-
-    return (
-        <main>
-            <Newsletter />
-        </main>
-    );
+  return (
+    <main>
+      {email ? (
+        <div className="successMessage">
+          <h2>Thanks for subscribing!</h2>
+          <p>A confirmation email has been sent to <b>{email}</b>. Please open it and click the button inside to confirm your subscription</p>
+        </div>
+      ) : (
+        <Newsletter setEmail={setEmail}/>
+      )}
+    </main>
+  );
 }
 
 export default App;
